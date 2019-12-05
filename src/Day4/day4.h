f998050e46f8f7e51a44ec8f35bd2b784ae21035
Reece Mercer <reecemercer@outlook.com>
2019-12-05 04:52:12 +0000

    Day 4 part 2

--- src/Day4/day4.h
+++ src/Day4/day4.h
@@ -11,7 +11,6 @@ const int LOWER = 231832;
 const int HIGHER = 767346;
 
 bool adjacent_digits(int potential_pwd);
-bool has_adj_equal_to_2_without_repeat(int potential_pwd);
 bool never_decrease(int potential_pwd);
 int day_4_part_1();
 int day_4_part_2();
